f35018b19ef24f8080b20f0f37d75c76637c0949
Panupanx <ball@reverseads.com>
2021-08-02 15:46:35 +0700

    create components and form

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log('You clicked submit.');
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
+import form from './components/form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="layout">
+      <div class="body-content">
+        <div className="in-content">
+          {form()}
+        </div>
+      </div>
+
+      <div class="body-content">
+      </div>
+
     </div>
   );
 }
